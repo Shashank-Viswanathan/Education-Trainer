0f3d1eb7de578fc1af4bbac6732246f4ceb33542
Shashank-Viswanathan <147416056+Shashank-Viswanathan@users.noreply.github.com>
2023-12-19 20:24:59 -0500

    Not done

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Loading resources...\n");
+    Scanner ID_input = new Scanner(System.in);
+    System.out.println("Enter your first name: ")
+    String first_name = ID_input.nextLine();
+  }
+}
